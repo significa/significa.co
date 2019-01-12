0e7e82e2043a272c056085d96b09d97bea0b7ea2
Pedro Brandão <pedro@significa.pt>
2019-01-12 19:13:16 +0000

    SI-68 Theme alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@theme': path.resolve(__dirname, 'src/theme'),
+      },
+      extensions: ['.ts', '.tsx'],
+    },
+  })
+}
---
+++ src/components/Button/styled.ts
@@ -0,0 +1,7 @@
+import styled from '@theme'
+
+const Button = styled.button`
+  background-color: red;
+`
+
+export { Button }

ddf3c67091c4c2a5bab3be613dfd123c1167d61b
Pedro Brandão <pedro@significa.pt>
2019-01-13 21:18:12 +0000

    SI-73 Add some comments

--- src/theme/components/Theme.tsx
+++ src/theme/components/Theme.tsx
@@ -1,3 +1,8 @@
+/**
+ * This component can be used to wrap sections with different themes.
+ * Can be nested and defines the theme for all the children.
+ */
+
 import React from 'react'
 
 import { StyledThemeProvider } from '../styled'

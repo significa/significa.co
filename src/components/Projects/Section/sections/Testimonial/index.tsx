fcbc1fd5c1b982350edfebc4886c0036ee0d5ef1
Pedro Brandão <pedro@significa.pt>
2019-01-30 20:11:53 +0100

    SI-71 Cleanup, highlight and testimonial

--- src/components/Projects/Section/sections/Testimonial/index.tsx
+++ src/components/Projects/Section/sections/Testimonial/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
+import { ITestimonial } from '../../../../../templates/types'
+import { textByLine } from '../../../../../utils/textByLine'
+
+import { Title, Text, ArrowLink } from '../../../../UI'
+
 import * as S from './styled'
 
-const Testimonial = () => <S.Wrapper>Testimonial section</S.Wrapper>
+const Testimonial = (props: ITestimonial) => {
+  return (
+    <S.Wrapper>
+      <S.Quote />
+      {textByLine(props.text).map((line, i) => (
+        <Title as="p" key={i}>
+          {line}
+        </Title>
+      ))}
+      <S.Meta>
+        <Text>{props.author}</Text>
+        {props.link && (
+          <ArrowLink to={props.link.url}>{props.link.text}</ArrowLink>
+        )}
+      </S.Meta>
+    </S.Wrapper>
+  )
+}
 
 export default Testimonial

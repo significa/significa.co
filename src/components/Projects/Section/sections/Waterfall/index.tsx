ab09cceb08d057353eba4d4f678742f9fc6e0e56
Pedro Brandão <pedro@significa.pt>
2019-01-31 22:04:34 +0100

    SI-71 Waterfall (To be redone)

--- src/components/Projects/Section/sections/Waterfall/index.tsx
+++ src/components/Projects/Section/sections/Waterfall/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import Columns from 'react-columns'
+
+import { IWaterfall } from '../../../../../templates/types'
 
 import * as S from './styled'
 
-const Waterfall = () => <S.Wrapper>Waterfall section</S.Wrapper>
+const Waterfall = (props: IWaterfall) => {
+  const columns = [
+    {
+      columns: 2,
+      query: 'min-width: 0',
+    },
+    {
+      columns: 1,
+      query: 'max-width: 32em',
+    },
+  ]
+
+  return (
+    <S.Wrapper>
+      <Columns queries={columns}>
+        {props.items.map((item, i) => (
+          <S.ImgHolder key={i}>
+            <Img key={i} fluid={item.image.childImageSharp.fluid} />
+          </S.ImgHolder>
+        ))}
+      </Columns>
+    </S.Wrapper>
+  )
+}
 
 export default Waterfall

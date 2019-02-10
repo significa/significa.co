16affd63d80449dd8b8ab0db3c3b029cd27c2ef7
Pedro Brandão <pedro@significa.pt>
2019-02-10 11:37:19 +0000

    SI-89 Hooks

--- src/components/Projects/Section/sections/Slideshow/index.tsx
+++ src/components/Projects/Section/sections/Slideshow/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Img from 'gatsby-image'
 
 import { ISlideshow } from '../../types'
@@ -7,62 +7,49 @@ import Arrow from './Arrow'
 
 import * as S from './styled'
 
-interface ISlideShowState {
-  index: number
-}
+const Slideshow: React.FC<ISlideshow> = ({ items }) => {
+  const [index, setIndex] = useState(0)
 
-class Slideshow extends React.Component<ISlideshow, ISlideShowState> {
-  state = { index: 0 }
-
-  handleNextSlide = () => {
-    const { items } = this.props
-
-    this.setState(state => ({
-      index:
-        state.index === items.length - 1 ? items.length - 1 : state.index + 1,
-    }))
+  const handleNextSlide = () => {
+    setIndex(index === items.length - 1 ? items.length - 1 : index + 1)
   }
 
-  handlePrevSlide = () => {
-    this.setState(state => ({ index: state.index <= 0 ? 0 : state.index - 1 }))
+  const handlePrevSlide = () => {
+    setIndex(index <= 0 ? 0 : index - 1)
   }
 
-  render() {
-    const { items } = this.props
-    const { index } = this.state
-    const translate = (index % items.length) * (100 / items.length)
+  const translate = (index % items.length) * (100 / items.length)
 
-    return (
-      <S.Wrapper>
-        <S.SlideWrapper>
-          <S.SlideInner
-            style={{
-              width: items.length * 100 + '%',
-              transform: `translateX(${-translate}%)`,
-            }}
-          >
-            {items.map((item, i) => {
-              return (
-                <Img
-                  style={{ width: '100%' }}
-                  key={i}
-                  fluid={item.image.childImageSharp.fluid}
-                />
-              )
-            })}
-          </S.SlideInner>
-        </S.SlideWrapper>
-        <S.Controls>
-          <S.Left onClick={this.handlePrevSlide}>
-            <Arrow />
-          </S.Left>
-          <S.Right onClick={this.handleNextSlide}>
-            <Arrow />
-          </S.Right>
-        </S.Controls>
-      </S.Wrapper>
-    )
-  }
+  return (
+    <S.Wrapper>
+      <S.SlideWrapper>
+        <S.SlideInner
+          style={{
+            width: items.length * 100 + '%',
+            transform: `translateX(${-translate}%)`,
+          }}
+        >
+          {items.map((item, i) => {
+            return (
+              <Img
+                style={{ width: '100%' }}
+                key={i}
+                fluid={item.image.childImageSharp.fluid}
+              />
+            )
+          })}
+        </S.SlideInner>
+      </S.SlideWrapper>
+      <S.Controls>
+        <S.Left onClick={handlePrevSlide}>
+          <Arrow />
+        </S.Left>
+        <S.Right onClick={handleNextSlide}>
+          <Arrow />
+        </S.Right>
+      </S.Controls>
+    </S.Wrapper>
+  )
 }
 
 export default Slideshow

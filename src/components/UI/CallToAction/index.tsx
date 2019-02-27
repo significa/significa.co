2b120f682febf13bd6902d7549c4a80282475003
Pedro Brandão <pedro@significa.pt>
2019-02-27 19:51:33 +0100

    SI-60 Remove unused imports

--- src/components/UI/CallToAction/index.tsx
+++ src/components/UI/CallToAction/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import * as S from './styled'
-import { Display, Big, ArrowLink } from '../'
+import { ArrowLink } from '../'
 
 interface ICallToAction {
   title: string

13b18604ab22c6d572312df675ed0dff4300430b
Pedro <pedro@significa.pt>
2019-03-29 19:44:18 +0100

    SI-62 What matters more content

--- src/components/Contact/WhatMatters/index.tsx
+++ src/components/Contact/WhatMatters/index.tsx
@@ -1,24 +1,59 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import { Theme } from '@theme'
 
 import * as S from './styled'
+import { textByLine } from '../../../utils/textByLine'
 
-const WhatMatters: React.FC<{}> = () => {
+interface IWhatMatters {
+  contactYaml: {
+    matters: {
+      title: string
+      text: string
+      more: string
+    }
+  }
+}
+
+const WhatMatters: React.FC<IWhatMatters> = ({
+  contactYaml: { matters: data },
+}) => {
   return (
     <Theme theme="dark">
       <S.Wrapper>
         <S.Container>
-          <S.Title>
-            We’re always on the lookout for something that matters.
-          </S.Title>
-          <S.SubText>
-            Passionate projects with bigger-than-themselves goals have a special
-            place here at Significa.
-          </S.SubText>
+          <S.Title>{data.title}</S.Title>
+          <S.SubText>{data.text}</S.SubText>
+          <S.MoreWrapper>
+            {textByLine(data.more).map((line, i) => (
+              <S.MoreContent key={i}>{line}</S.MoreContent>
+            ))}
+          </S.MoreWrapper>
         </S.Container>
       </S.Wrapper>
     </Theme>
   )
 }
 
-export default WhatMatters
+const ConnectedWhatMatters = () => {
+  return (
+    <StaticQuery
+      query={whatMattersQuery}
+      render={(data: IWhatMatters) => <WhatMatters {...data} />}
+    />
+  )
+}
+
+const whatMattersQuery = graphql`
+  query WhatMattersQuery {
+    contactYaml {
+      matters {
+        title
+        text
+        more
+      }
+    }
+  }
+`
+
+export default ConnectedWhatMatters

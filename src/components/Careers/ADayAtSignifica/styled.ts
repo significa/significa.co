715100908aad3fee0246d74d589a0a7acb3e7c3d
Pedro <pedro@significa.pt>
2019-04-25 19:40:30 +0200

    SI-62 Top gallery

--- src/components/Careers/ADayAtSignifica/styled.ts
+++ src/components/Careers/ADayAtSignifica/styled.ts
@@ -7,8 +7,6 @@ export const Wrapper = styled(Container)`
   margin-top: 12em;
 
   @media (max-width: 64em) {
-    padding-left: 0;
-    padding-right: 0;
     margin-top: 3em;
   }
 `
@@ -34,45 +32,104 @@ export const Day = styled.div`
   width: 100%;
 `
 
-export const GalleryImage = styled(Img)<{ width: number; height: number }>`
-  margin-bottom: 5rem;
-  margin-right: 5rem;
-  flex-shrink: 0;
+export const TopGallery = styled.div`
+  display: grid;
+  grid-template: 4fr 5fr / repeat(8, 1fr);
+  grid-gap: 5em;
 
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => height}px;
+  @media (max-width: 64em) {
+    grid-gap: 2em;
+  }
 
-  &:last-child {
-    margin-right: 0;
+  @media (max-width: 48em) {
+    grid-template: repeat(2, 15em) / repeat(2, 1fr);
+  }
+
+  @media (max-width: 32em) {
+    grid-template-columns: 1fr;
   }
 `
 
-export const TopGallery = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+export const TopImage = styled(Img)`
+  &:nth-child(1) {
+    margin-left: 5em;
+    margin-top: 10em;
+    grid-column-end: span 2;
+  }
 
-  padding-bottom: 15vw;
+  &:nth-child(2) {
+    grid-column-end: span 2;
+  }
 
-  ${GalleryImage} {
-    &:nth-child(-n + 3) {
-      align-self: flex-end;
+  &:nth-child(3) {
+    grid-column-end: span 3;
+    margin-top: 5em;
+  }
+
+  &:nth-child(4) {
+    grid-column-end: span 3;
+    margin-bottom: 5em;
+  }
+
+  &:nth-child(5) {
+    grid-column-end: span 3;
+  }
+
+  &:nth-child(6) {
+    grid-column-end: span 2;
+    margin-bottom: 10em;
+  }
+
+  @media (max-width: 64em) {
+    &:nth-child(1) {
+      margin-left: 2em;
+      margin-top: 3em;
     }
 
-    @media (max-width: 64em) {
-      &:first-child,
-      &:nth-child(n + 4) {
-        display: none;
-      }
-
-      margin-bottom: 0;
-      margin-right: 0;
-      width: 100%;
+    &:nth-child(3) {
+      margin-top: 2em;
     }
 
-    @media (max-width: 32em) {
-      width: auto;
-      height: auto;
+    &:nth-child(4) {
+      margin-bottom: 2em;
+    }
+
+    &:nth-child(6) {
+      margin-bottom: 3em;
+    }
+  }
+
+  @media (max-width: 48em) {
+    &:nth-child(1n) {
+      margin: 0;
+      grid-column-end: span 1;
+    }
+
+    &:nth-child(3n - 2) {
+      display: none;
+    }
+
+    &:nth-child(2) {
+      margin-left: 3em;
+    }
+
+    &:nth-child(3) {
+      margin-top: 5em;
+      margin-right: 3em;
+    }
+
+    &:nth-child(6) {
+      margin-bottom: 3em;
+    }
+  }
+
+  @media (max-width: 32em) {
+    &:nth-child(1n) {
+      margin: 0;
+    }
+
+    &:nth-child(2n) {
+      display: none;
     }
   }
 `

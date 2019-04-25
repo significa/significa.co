7415109b6334f9733770a43cf89223935bfafc9d
Pedro <pedro@significa.pt>
2019-04-25 20:13:36 +0200

    SI-62 Logo sun

--- src/components/Careers/ADayAtSignifica/styled.ts
+++ src/components/Careers/ADayAtSignifica/styled.ts
@@ -11,24 +11,20 @@ export const Wrapper = styled(Container)`
   }
 `
 
-export const DayWrapper = styled.div`
+export const RelativeWrapper = styled.div`
   position: relative;
+  padding-bottom: 20vw;
 
-  @media (max-width: 64em) {
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-  }
-
-  path {
-    transition: strokeColor ${({ theme }) => theme.transitions.ease('400ms')};
+  @media (max-width: 48em) {
+    padding-bottom: 0;
   }
 `
 
 export const Day = styled.div`
   position: absolute;
   z-index: 100;
-  bottom: 71%;
-  left: -5.15%;
+  bottom: 0;
+  left: 0;
   width: 100%;
 `
 
@@ -130,30 +126,6 @@ export const TopImage = styled(Img)`
 
     &:nth-child(2n) {
       display: none;
-    }
-  }
-`
-
-export const BottomGallery = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-
-  padding-top: 5vw;
-
-  ${GalleryImage} {
-    &:nth-child(-n + 2) {
-      align-self: flex-end;
-    }
-
-    @media (max-width: 64em) {
-      &:nth-child(n + 3) {
-        display: none;
-      }
-
-      margin-bottom: 1.5rem;
-      margin-right: 0;
-      width: 100%;
     }
   }
 `

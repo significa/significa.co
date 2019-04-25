a75f60ce6aed6041c5e774f1ac2981a5055430ab
Pedro <pedro@significa.pt>
2019-04-25 12:01:41 +0200

    SI-62 Update eggs

--- src/components/Careers/Perks/Eggs/Flexible.tsx
+++ src/components/Careers/Perks/Eggs/Flexible.tsx
@@ -1,67 +1,68 @@
 import React from 'react'
 
-const Flexible = () => {
-  return (
-    <svg width="122" height="116" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fillRule="evenodd">
-        <path
-          d="M80.716 31.301C72.076.374 48.042 1.297 48.042 1.297S18.025.144 13.023 43.579c0 0-3.848 41.512 35.019 41.512 17.103 0 25.935-8.039 30.463-17.042"
-          stroke="currentColor"
-          strokeWidth="2.57"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          stroke="currentColor"
-          strokeWidth="2.57"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M35.685 83.418l.887 29.427 6.763 1.674M60.921 83.418l.887 29.427 6.763 1.674"
-        />
-        <path
-          d="M47.938 1.294s-4.202 24.224-30.813 26.368M45.147 10.617s10.444 9.79 31.24 9.748M22.15 15.793S6.674 4.898 1.298 29.14M7.236 25.274l-.951 6.29M11.724 25.274v6.86"
-          stroke="currentColor"
-          strokeWidth="2.573"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M55.757 36.418h13.609c.526 0-1.171 7.383-3.717 8.44-.659.274-1.503.304-2.298.304-4.413 0-8.063-3.25-8.676-7.48a1.098 1.098 0 0 1 1.082-1.264zm3.95 5.497s1.143 1.33 3.618 1.33c2.477 0 3.43-1.33 3.43-1.33s-1.334-1.424-3.43-1.424c-2.095 0-3.618 1.424-3.618 1.424z"
-          fill="currentColor"
-        />
-        <path
-          stroke="currentColor"
-          strokeWidth="2.276"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M56.98 67.824H34.24v-4.73h23.886"
-        />
-        <path
-          stroke="currentColor"
-          strokeWidth="2.276"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M70.932 31.645h49.216l-15.175 36.178H56.98z"
-        />
-        <path
-          d="M93.25 51.33c-.8 1.906-2.722 2.923-4.293 2.267-1.57-.656-2.193-2.734-1.392-4.641.8-1.908 2.722-2.923 4.292-2.268 1.572.655 2.194 2.733 1.394 4.642"
-          fill="currentColor"
-          opacity="0.1"
-        />
-        <path
-          d="M54.468 31.645a2.468 2.468 0 0 1-2.471 2.465 2.468 2.468 0 0 1-2.472-2.465 2.47 2.47 0 0 1 2.472-2.465 2.468 2.468 0 0 1 2.47 2.465M70.133 34.11a2.468 2.468 0 0 1-2.47-2.465 2.468 2.468 0 0 1 2.47-2.465 2.468 2.468 0 0 1 2.472 2.465l-2.472 2.465z"
-          fill="currentColor"
-        />
-        <path
-          d="M24.987 40.49S13.28 91.624 50.232 68.134M78.225 68.06s3.224 12.756 15.251.173"
-          stroke="currentColor"
-          strokeWidth="2.57"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
-    </svg>
-  )
-}
+const Flexible = () => (
+  <svg width={122} height={116}>
+    <g fill="none" fillRule="evenodd">
+      <path
+        d="M80.77 31.494C72.176.37 48.042 1.297 48.042 1.297S18.025.144 13.023 43.579c0 0-3.848 41.512 35.019 41.512 17.19 0 26.024-8.12 30.531-17.177"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M35.685 83.418l.887 29.427 6.763 1.674M60.921 83.418l.887 29.427 6.763 1.674M47.938 1.294s-4.202 24.224-30.813 26.368M45.147 10.617s10.444 9.79 31.24 9.748M22.15 15.793S6.674 4.898 1.298 29.14M7.236 25.274l-.951 6.29M11.724 25.274v6.86"
+      />
+      <path
+        d="M58.707 41.915s1.523-1.424 3.618-1.424c2.096 0 3.43 1.424 3.43 1.424s-.953 1.33-3.43 1.33c-2.475 0-3.618-1.33-3.618-1.33zm-3.95-5.497c-.672 0-1.18.599-1.082 1.263.613 4.231 4.263 7.48 8.676 7.48 1.27 0 2.475-.268 3.564-.752l3.096-7.991H54.757z"
+        fill="currentColor"
+      />
+      <path
+        fill="currentColor"
+        opacity={0.05}
+        d="M56.98 67.824H34.24v-4.73h25.172"
+      />
+      <path
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M56.98 67.824H34.24v-4.73h24.172"
+      />
+      <path
+        fill="currentColor"
+        opacity={0.05}
+        d="M70.932 31.645h49.216l-15.175 36.178H56.98z"
+      />
+      <path
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M70.932 31.645h49.216l-15.175 36.178H56.98z"
+      />
+      <path
+        d="M93.25 51.33c-.8 1.906-2.722 2.923-4.293 2.267-1.57-.656-2.193-2.734-1.392-4.641.8-1.908 2.722-2.923 4.292-2.268 1.572.655 2.194 2.733 1.394 4.642"
+        fill="currentColor"
+        opacity={0.1}
+      />
+      <path
+        d="M54.468 30.645a2.468 2.468 0 0 1-2.471 2.465 2.468 2.468 0 0 1-2.472-2.465 2.47 2.47 0 0 1 2.472-2.465 2.468 2.468 0 0 1 2.47 2.465M72.605 30.645a2.468 2.468 0 0 1-2.472 2.465 2.468 2.468 0 0 1-2.47-2.465 2.468 2.468 0 0 1 2.47-2.465 2.468 2.468 0 0 1 2.472 2.465"
+        fill="currentColor"
+      />
+      <path
+        d="M24.987 40.49S13.28 91.624 50.232 68.134M78.225 68.06s3.224 12.756 15.251.173"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+  </svg>
+)
 
 export default Flexible

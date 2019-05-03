549e196b3c8134a0c338c41e9b777cc4586b2c48
Pedro <pedro@significa.pt>
2019-05-03 11:40:06 -0700

    Improve sticky header

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -26,9 +26,16 @@ export const Wrapper = styled.header`
 `
 
 export const Header = styled(Headroom)`
+  background-color: ${({ theme }) => theme.colors.background};
+
+  .headroom {
+    z-index: 999 !important;
+  }
   .headroom--pinned {
     ${Wrapper} {
-      padding-top: 1em;
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), 0 2px 8px rgba(0, 0, 0, 0.05);
     }
   }
 `

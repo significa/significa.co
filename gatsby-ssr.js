ae007f2c1074978fdda156c5d8d179af5b1bff12
Pedro <pedro@significa.pt>
2019-05-03 18:01:03 -0700

    Add crisp

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,5 +3,28 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+import React from 'react'
 
-// You can delete this file if you're not using it
+const CRISP_ID = '1d94ab70-6906-4198-a006-16144d4c8065'
+
+const onRenderBody = ({ setHeadComponents }) => {
+  if (process.env.NODE_ENV === 'development') {
+    return null
+  }
+
+  const scriptInnerHTML =
+    `window.$crisp=[];window.CRISP_WEBSITE_ID="${CRISP_ID}";` +
+    `(function(){d=document;s=d.createElement("script");` +
+    `s.src="https://client.crisp.chat/l.js";` +
+    `s.async=1;` +
+    `d.getElementsByTagName("head")[0].appendChild(s);})();`
+
+  return setHeadComponents([
+    <script
+      key="crisp-chat"
+      dangerouslySetInnerHTML={{ __html: scriptInnerHTML }}
+    />,
+  ])
+}
+
+export { onRenderBody }

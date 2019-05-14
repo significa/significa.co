026f5819813d04bad5e3ddb884731f5b2a1cf00a
Danilo Woznica <danilowoz@gmail.com>
2019-05-14 19:33:19 +0100

    feat(about): split flap animation

--- src/components/About/Global/WorldMap/index.tsx
+++ src/components/About/Global/WorldMap/index.tsx
@@ -4,10 +4,25 @@ import { graphql, useStaticQuery } from 'gatsby'
 import worldMapSource from './world-map.svg'
 import * as S from './styled'
 
-const TIME_LETTER = 50
-const TIME_RANDOM_CITY = 5000
+interface IPanel {
+  current: string
+  old: string
+}
+
+const TIME_LETTER = 150
+const TIME_RANDOM_CITY = 6000
 
 const template = (city: string) => `OPO>${city}`
+const initialState = () =>
+  new Array(7).fill(' ').map((_, index) => {
+    const randomLetter = char[Math.floor(Math.random() * char.length)]
+
+    if (index === 3) {
+      return { current: '>', old: '>' }
+    }
+
+    return { current: randomLetter, old: '0' }
+  })
 
 const WorldMap = () => {
   const {
@@ -16,44 +31,41 @@ const WorldMap = () => {
     },
   } = useStaticQuery(query)
 
-  const initialState = () =>
-    new Array(7).fill(' ').map((_, index) => {
-      if (index === 3) {
-        return '>'
-      }
-
-      return char[Math.floor(Math.random() * char.length)]
-    })
-
+  const [splitFlap, setSplitFlap] = React.useState(initialState())
   const [currentCity, setCurrentCity] = React.useState(
     template(cities[Math.floor(Math.random() * cities.length)])
   )
-  const [splitFlap, setSplitFlap] = React.useState(initialState())
 
+  //
   // Pass throught all letters
-  const flipPanel = (curr: string[]) => {
+  //
+  const flipPanel = (curr: IPanel[]) => {
     const splittedCity = currentCity.split('')
-    const newSorted = curr.map((l, index) => {
-      if (l === splittedCity[index]) {
-        return l
+    const newSorted = curr.map(({ current }, index) => {
+      if (current === splittedCity[index]) {
+        return { current, old: current }
       }
 
-      const indexChart = (char.indexOf(l) + 1) % char.length
+      const indexChart = (char.indexOf(current) + 1) % char.length
 
-      return char[indexChart]
+      return { current: char[indexChart], old: current }
     })
 
     return setSplitFlap(newSorted)
   }
 
+  //
   // Random city
+  //
   const randomCity = () => {
     const city = cities[Math.floor(Math.random() * cities.length)]
     setCurrentCity(template(city))
     setSplitFlap(initialState())
   }
 
+  //
   // Call to update the letters
+  //
   React.useEffect(() => {
     const interval = setTimeout(() => flipPanel(splitFlap), TIME_LETTER)
 
@@ -73,9 +85,33 @@ const WorldMap = () => {
       <S.MapImg src={worldMapSource} alt="World map" />
 
       <S.BasePanel>
-        {splitFlap.map((l, index) => (
-          <S.Panel key={`${l}-${index}`}>
-            <span>{l}</span>
+        {splitFlap.map(({ current, old }, index) => (
+          <S.Panel key={`${current}-${index}`}>
+            <S.HalfPanel position="top">
+              <div>{current}</div>
+            </S.HalfPanel>
+            <S.HalfPanel position="bottom">
+              <div>{old}</div>
+            </S.HalfPanel>
+
+            {current !== old && (
+              <>
+                <S.HalfFlipPanel
+                  time={TIME_LETTER}
+                  direction="out"
+                  position="top"
+                >
+                  <div>{old}</div>
+                </S.HalfFlipPanel>
+                <S.HalfFlipPanel
+                  time={TIME_LETTER}
+                  direction="in"
+                  position="bottom"
+                >
+                  <div>{current}</div>
+                </S.HalfFlipPanel>
+              </>
+            )}
           </S.Panel>
         ))}
       </S.BasePanel>
--- src/components/About/Global/WorldMap/styled.ts
+++ src/components/About/Global/WorldMap/styled.ts
@@ -1,4 +1,5 @@
 import styled, { media, colors } from '@theme'
+import { keyframes, css } from 'styled-components'
 
 export const MapWrapper = styled.div`
   padding-top: 4em;
@@ -42,6 +43,9 @@ export const Panel = styled.div`
   background: ${({ theme }) => theme.colors.background};
   color: ${({ theme }) => theme.colors.foreground};
   box-sizing: border-box;
+  position: relative;
+  transform: perspective(600px);
+  overflow: hidden;
 
   display: inline-flex;
   align-items: center;
@@ -53,6 +57,16 @@ export const Panel = styled.div`
   width: 5rem;
   height: 6rem;
   border-radius: 2px;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 100%;
+    border: 1px solid ${colors.lightBlack};
+    position: absolute;
+    top: calc(50% + 3px);
+    z-index: 99;
+  }
 
   &:not(:last-child) {
     margin-right: 0.5rem;
@@ -93,5 +107,87 @@ export const Panel = styled.div`
     &:nth-child(3) {
       margin-bottom: 0.5rem;
     }
+  }
+`
+
+export const HalfPanel = styled.div<{ position: string }>`
+  ${({ theme, position }) => {
+    const isTop = position === 'top'
+
+    return css`
+      position: ${isTop ? 'relative' : 'absolute'};
+      overflow: hidden;
+      width: 100%;
+      text-align: center;
+
+      div {
+        background: ${isTop
+          ? `linear-gradient(${theme.colors.foreground} 50%, transparent 50%)`
+          : `linear-gradient(transparent 50%, ${theme.colors.foreground} 50%)`};
+
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    `
+  }}
+`
+
+export const HalfFlipPanel = styled(HalfPanel)<{
+  direction: string
+  position: string
+  time: number
+}>`
+  ${({ direction, theme, time, position }) => {
+    const isIn = direction === 'in'
+    const isBottom = position === 'bottom'
+
+    return css`
+      position: absolute;
+      animation-name: ${isIn ? FlipIn : FlipOut};
+      animation-timing-function: linear;
+      animation-fill-mode: forwards;
+      animation-duration: ${time}ms;
+      background: ${isIn
+        ? `linear-gradient(transparent 50%, ${theme.colors.background} 50%)`
+        : `linear-gradient(${theme.colors.background} 50%, transparent 50%)`};
+      opacity: 1;
+
+      ${isBottom && `box-shadow: 0 2px 20px 10px ${theme.colors.background}`};
+
+      div {
+        background: ${isBottom
+          ? `linear-gradient(transparent 50%, ${theme.colors.foreground} 50%)`
+          : `linear-gradient(${theme.colors.foreground} 50%, transparent 50%)`};
+
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    `
+  }}
+`
+
+const FlipIn = keyframes`
+  0% {
+    transform: rotateX(-90deg);
+  }
+  50% {
+    transform: rotateX(-90deg);
+  }
+  100% {
+    transform: rotateX(0deg);
+  }
+`
+
+const FlipOut = keyframes`
+  0% {
+    transform: rotateX(0deg);
+  }
+  50% {
+    transform: rotateX(90deg);
+  }
+  100% {
+    transform: rotateX(90deg);
   }
 `

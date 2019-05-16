9e46574b58941c465c36526668ce59921371401b
Pedro Brandão <pedro@significa.pt>
2019-05-16 12:43:08 -0700

    #73 Add yml margin to chapters

--- src/components/Projects/Chapter/styled.ts
+++ src/components/Projects/Chapter/styled.ts
@@ -1,4 +1,29 @@
-import styled from '@theme'
+import styled, { media, css } from '@theme'
+import { marginTypes } from '../Section/types'
+
+export const getMargins = (p: { margin?: marginTypes }) => {
+  if (p.margin && p.margin !== 'none') {
+    const hasTop = p.margin === 'top' || p.margin === 'both'
+    const hasBottom = p.margin === 'bottom' || p.margin === 'both'
+
+    return css`
+      margin-top: ${hasTop ? '5em' : 0};
+      margin-bottom: ${hasBottom ? '5em' : 0};
+
+      ${media.medium} {
+        margin-top: ${hasTop ? '4em' : 0};
+        margin-bottom: ${hasBottom ? '4em' : 0};
+      }
+
+      ${media.small} {
+        margin-top: ${hasTop ? '3em' : 0};
+        margin-bottom: ${hasBottom ? '3em' : 0};
+      }
+    `
+  }
+
+  return css``
+}
 
 export const Wrapper = styled.div`
   display: flex;
@@ -12,4 +37,6 @@ export const Wrapper = styled.div`
   min-height: 50vh;
 
   background-color: ${({ theme }) => theme.colors.background};
+
+  ${getMargins};
 `

20f9d33b8bbbabe2ada6a3c73b7802045f777819
Pedro Brandão <pedro@significa.co>
2019-07-30 10:09:06 +0100

    Update header

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -36,7 +36,7 @@ export const Header = styled(Headroom)`
     ${Wrapper} {
       padding-top: 0.75rem;
       padding-bottom: 0.75rem;
-      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), 0 2px 8px rgba(0, 0, 0, 0.05);
+      box-shadow: 0 1px 0px rgba(0, 0, 0, 0.05);
     }
   }
 `

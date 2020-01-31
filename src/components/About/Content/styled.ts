8067f0ea415edc8d45e3318a3f04763be7eee20a
Pedro Brandão <pedro@significa.co>
2020-01-31 14:27:43 +0000

    fix: remove old page

--- src/components/About/Content/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-import { Text as BaseText } from '../../UI'
-
-export const Text = styled(BaseText)`
-  &:not(:last-child) {
-    margin-bottom: 1em;
-  }
-`
--- src/components/About2/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Top } from './Top/Top'
-export { default as Services } from './Services/Services'
-export { default as Handbook } from './Handbook/Handbook'
-export { default as Team } from './Team/Team'
-export { default as Office } from './Office/Office'

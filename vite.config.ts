452939c0157588f0f6811325f7a877a64ab1549b
Pedro Brandão <pedro@significa.co>
2023-02-15 16:10:37 +0000

    chore: more config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit(), mkcert()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  server: {
+    https: true
+  }
 });

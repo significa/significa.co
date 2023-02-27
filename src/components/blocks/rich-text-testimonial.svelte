d90e9db378702943f6be16c94fba6b900aae7372
Pedro Brandão <pedro@significa.co>
2023-02-27 21:04:53 +0000

    refactor: testimonial

--- src/components/blocks/rich-text-testimonial.svelte
+++ src/components/blocks/rich-text-testimonial.svelte
@@ -7,6 +7,7 @@
 
 <div class="testimonial">
   {#if block.testimonial}
+    <p class="symbol">&ldquo;</p>
     <p class="quote"><span>{block.testimonial}</span></p>
   {/if}
   <div class="info">
@@ -27,6 +28,8 @@
 
 <style lang="postcss">
   div.testimonial {
+    position: relative;
+
     --richtext-testimonial-padding: 10px 0px;
 
     @media (--md) {
@@ -34,45 +37,57 @@
     }
 
     padding: var(--richtext-testimonial-padding);
-  }
 
-  p.quote {
-    font-size: var(--font-size-2xl);
-    line-height: var(--line-height-2xl);
-    font-weight: var(--font-weight-medium);
-
-    & span {
-      background-color: hsl(var(--color-outline-hsl) / 0.5);
-      border-radius: var(--border-radius-2xs);
-    }
-  }
-
-  .info {
-    display: flex;
-    align-items: center;
-
-    & p.name,
-    & p.position {
-      font-size: var(--font-size-md);
-      line-height: var(--line-height-none);
+    & p.quote {
+      font-size: var(--font-size-2xl);
+      line-height: var(--line-height-2xl);
       font-weight: var(--font-weight-medium);
     }
 
-    & p.name {
-      margin-bottom: 0.5ch;
+    & p.symbol {
+      font-size: 6rem;
+      line-height: 0;
+      color: var(--color-foreground-accent);
+
+      margin-bottom: 24px;
+      margin-top: 32px;
+
+      @media (--md) {
+        line-height: 1;
+        margin-top: 0;
+        position: absolute;
+        left: 0;
+        top: 0;
+      }
     }
 
-    & p.position {
-      color: var(--color-foreground-secondary);
+    & .info {
+      display: flex;
+      align-items: center;
+
+      & p.name,
+      & p.position {
+        font-size: var(--font-size-md);
+        line-height: var(--line-height-none);
+        font-weight: var(--font-weight-medium);
+      }
+
+      & p.name {
+        margin-bottom: 0.5ch;
+      }
+
+      & p.position {
+        color: var(--color-foreground-secondary);
+      }
     }
-  }
 
-  .avatar {
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin-right: 1ch;
+    & .avatar {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin-right: 1ch;
 
-    flex-shrink: 0;
+      flex-shrink: 0;
+    }
   }
 </style>

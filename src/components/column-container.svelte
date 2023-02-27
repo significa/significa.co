a7112b023b82f093e0883cc6db4cf74d56a5ea50
Pedro Brandão <pedro@significa.co>
2023-02-27 21:07:36 +0000

    chore: remove comment

--- src/components/column-container.svelte
+++ src/components/column-container.svelte
@@ -8,38 +8,6 @@
     margin-inline: auto;
     padding-inline: var(--container-padding-inline);
 
-    /* This grid approach doesn't work with collapsing margins */
-    /* @media (--md) {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr) min(660px, 100%) repeat(3, 1fr);
-      column-gap: 16px;
-    }
-
-    & :global(*) {
-      grid-column: 4 / 5;
-    }
-
-    & :global([data-column-width='medium']) {
-      grid-column: 4 / 5;
-
-      @media (--lg) {
-        grid-column: 3 / 6;
-      }
-    }
-
-    & :global([data-column-width='wide']) {
-      grid-column: 1 / -1;
-
-      @media (--lg) {
-        grid-column: 2 / 7;
-      }
-    }
-
-    & :global([data-column-width='full']) {
-      grid-column: 1 / -1;
-    } */
-
-    /* This approach works with collapsing margins */
     & > :global(*) {
       width: min(660px, 100%);
       margin-inline: auto;

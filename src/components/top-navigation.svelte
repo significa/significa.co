c128c7fd73c914aeccf4f260a95527d9a0c540d3
Pedro Brandão <pedro@significa.co>
2023-03-13 11:04:35 +0000

    feat: top nav layout when in handbook

--- src/components/top-navigation.svelte
+++ src/components/top-navigation.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import { Button, Logo } from '@significa/svelte-ui';
+  import clsx from 'clsx';
 </script>
 
-<header class="container flex items-center justify-between py-4">
+<header
+  class={clsx(
+    'flex items-center justify-between py-4',
+    // we could achieve this with route groups and a different layout, but it's so simple that we'll leave it like this for now
+    $page.url.pathname.startsWith('/handbook') ? 'px-container' : 'container'
+  )}
+>
   <Logo class="mt-1" variant="wordmark" />
 
   <div class="flex items-center gap-4">

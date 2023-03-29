4a3d53e781f82442037d2c0568a06b8e5c12bdb3
Pedro Brandão <pedro@significa.co>
2023-03-29 22:41:33 +0100

    refactor: manual ease

--- src/components/pages/get-a-quote/images.svelte
+++ src/components/pages/get-a-quote/images.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
-  import { spring } from 'svelte/motion';
+  import { onMount } from 'svelte';
   import img1 from '../../../assets/get-a-quote-1.png';
   import img2 from '../../../assets/get-a-quote-2.png';
   import img3 from '../../../assets/get-a-quote-3.png';
   import img4 from '../../../assets/get-a-quote-4.png';
-  import { writable } from 'svelte/store';
 
   const images = [img1, img2, img3, img4];
 
-  let el: HTMLDivElement;
-  const y = writable(0);
-  const eased = spring(0, {
-    stiffness: 0.1,
-    damping: 0.4
+  let y = 0;
+  let eased = 0;
+
+  const ease = () => {
+    eased = eased + (y - eased) * 0.075;
+    frame = window.requestAnimationFrame(ease);
+  };
+
+  let frame: number;
+  onMount(() => {
+    frame = window.requestAnimationFrame(ease);
+    return () => {
+      window.cancelAnimationFrame(frame);
+    };
   });
-  $: eased.set($y);
 </script>
 
-<svelte:window bind:scrollY={$y} />
+<svelte:window bind:scrollY={y} />
 
-<div bind:this={el} class="flex justify-center overflow-hidden border-b py-12 md:py-20 lg:py-32">
-  <div class="flex gap-10" style="transform: translateX(calc({$eased / 5}px * -1 + 15vw))">
+<div class="flex justify-center overflow-hidden border-b py-12 md:py-20 lg:py-32">
+  <div class="flex gap-10" style="transform: translateX(calc({eased / 5}px * -1 + 15vw))">
     {#each images as img}
       <img class="max-h-96" alt="" src={img} />
     {/each}

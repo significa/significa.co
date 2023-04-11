a450aa6df62862f8fdf84b8633c804b70bf44eea
Pedro Brandão <pedro@significa.co>
2023-04-11 11:11:12 +0100

    refactor: drawer with query params

--- src/components/page-drawer.svelte
+++ src/components/page-drawer.svelte
@@ -5,25 +5,15 @@
   import { drawer } from '$lib/stores/drawer';
   import { Button, Spinner } from '@significa/svelte-ui';
   import { fade, fly } from 'svelte/transition';
-  import { items } from './image-gallery.svelte';
   import { beforeNavigate } from '$app/navigation';
   import { browser } from '$app/environment';
   import { page } from '$app/stores';
   import { fetchPage } from '$lib/content';
 
+  let expanding = false;
   beforeNavigate(() => {
     if ($drawer) drawer.close();
   });
-
-  const onkeydown = (e: KeyboardEvent) => {
-    if (!$drawer) return;
-
-    // not very elegant, but if the image gallery is open, ESC should close it (and not the drawer)
-    // we should probably have a centralized way to handle this, instead of ad-hoc solutions for every possible element on top of the drawer
-    if ($items.length) return;
-
-    if (e.key === 'Escape') drawer.close();
-  };
 </script>
 
 {#if $drawer && browser}
@@ -31,7 +21,9 @@
     use:escapeKey={{ id: 'page-drawer', callback: drawer.close }}
     use:bodyLock
     role="button"
-    on:keydown={onkeydown}
+    on:keydown={(event) => {
+      if (event.key === 'Escape') drawer.close();
+    }}
     transition:fade={{ duration: 200 }}
     class="fixed inset-0 z-50 bg-black/50"
     on:click={drawer.close}
@@ -39,7 +31,7 @@
   <div
     class="fixed bottom-2 left-2 right-2 top-2 z-50 overflow-y-auto scroll-smooth rounded-lg bg-background shadow-2xl md:left-auto md:w-3/4 lg:w-2/3"
     in:fly={{ x: 300, duration: 200 }}
-    out:fly={{ x: -300, duration: 100 }}
+    out:fly={{ x: expanding ? -300 : 300, duration: expanding ? 200 : 100 }}
   >
     <header class="z-50 flex items-center justify-between p-2">
       <Button
@@ -48,7 +40,10 @@
         class="bg-background"
         variant="ghost"
         icon="expand"
-        on:click={drawer.close}>{t('expand')}</Button
+        on:click={() => {
+          expanding = true;
+          drawer.close();
+        }}>{t('expand')}</Button
       >
       <Button class="bg-background" variant="ghost" icon="close" on:click={drawer.close} />
     </header>

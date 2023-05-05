a2f167b4755838ff4135d43f08b659f08c3411e4
Filipe Ferreira <filipeferreira@significa.co>
2023-05-05 17:11:08 +0100

    feat(actions): dragscrolling reusable action

--- src/components/pages/careers/canvas.svelte
+++ src/components/pages/careers/canvas.svelte
@@ -11,6 +11,7 @@
   import Checklist from './canvas-items/checklist.svelte';
   import Tictactoe from './canvas-items/tictactoe.svelte';
   import type { TeamMemberPage } from '$lib/content';
+  import { dragScrolling } from '$lib/actions/drag-scrolling';
 
   export let withMouseDragScroll = false;
   export let title: string | undefined = undefined;
@@ -21,68 +22,13 @@
 
   let offsetX = (width || 0) / 2;
   let offsetY = (height || 0) / 2;
-
-  const dragScrolling = (node: HTMLElement) => {
-    if (withMouseDragScroll) {
-      // state
-      let dragging = false;
-
-      // overflow hidden allows forced scroll which is really nice
-      node.style.overflow = 'hidden';
-      node.style.cursor = 'grab';
-
-      // center
-      node.scrollLeft = (node.scrollWidth - node.clientWidth) / 2;
-      node.scrollTop = (node.scrollHeight - 240) / 2;
-
-      // save initial state
-      let left = node.scrollLeft;
-      let top = node.scrollTop;
-
-      node.addEventListener('mousedown', () => {
-        dragging = true;
-        node.style.cursor = 'grabbing';
-        node.style.userSelect = 'none';
-      });
-
-      node.addEventListener('mousemove', (e) => {
-        if (dragging) {
-          left -= e.movementX;
-          top -= e.movementY;
-
-          node.scrollLeft = left;
-          node.scrollTop = top;
-        }
-      });
-
-      node.addEventListener('mouseup', () => {
-        dragging = false;
-        node.style.cursor = 'grab';
-        node.style.userSelect = 'unset';
-      });
-
-      window.addEventListener('resize', () => {
-        node.scrollLeft = (node.scrollWidth - node.clientWidth) / 2;
-        node.scrollTop = (node.scrollHeight - 240) / 2;
-      });
-
-      // prevent stuck drag when leaving node still pressing
-      node.addEventListener('mouseleave', () => {
-        dragging = false;
-        node.style.cursor = 'grab';
-        node.style.userSelect = 'unset';
-      });
-    } else {
-      node.style.overflow = 'auto';
-
-      // center
-      node.scrollLeft = (node.scrollWidth - node.clientWidth) / 2;
-      node.scrollTop = (node.scrollHeight - 240) / 2;
-    }
-  };
 </script>
 
-<div use:dragScrolling style={$$restProps.style} class={$$restProps.class}>
+<div
+  use:dragScrolling={{ isActive: withMouseDragScroll, centerYOffset: -240 }}
+  style={$$restProps.style}
+  class={$$restProps.class}
+>
   <div
     class={clsx('relative mx-auto', $$restProps.class)}
     style="width: {width || 0}px; height: {height ||

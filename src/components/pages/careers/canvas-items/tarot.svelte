92b1a707fc6db57ab816f2659a08a0f25a26c4cf
Michelle Meinert <michelle@significa.co>
2023-05-07 11:25:04 +0200

    style: wiggle animation

--- src/components/pages/careers/canvas-items/tarot.svelte
+++ src/components/pages/careers/canvas-items/tarot.svelte
@@ -9,17 +9,22 @@
   export let asset;
   let flip = false;
   import back from '$assets/back.svg';
+  setTimeout(() => {
+    flip = false;
+  }, 4000);
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="flip-card" on:click={() => (flip = !flip)}>
-  <div class="flip-card-inner" class:rotate={flip}>
+  <div class="rleative flip-card-inner h-full w-full" class:rotate={flip}>
     <div class="flip-card-front">
       <img alt="" src={asset} class="drop-shadow-sm" />
     </div>
     <div class="flip-card-back">
       <div class="relative">
-        <p class="absolute left-3 top-6 z-10 text-left text-xl font-semibold text-[#171717]">
+        <p
+          class="absolute left-6 top-6 z-10 max-w-[154px] text-left text-xl font-semibold text-[#171717]"
+        >
           {text}
         </p>
         <img alt="" src={back} class="drop-shadow-sm" />
@@ -33,15 +38,10 @@
     background-color: transparent;
     width: 200px;
     height: 300px;
-    perspective: 1000px; /* Remove this if you don't want the 3D effect */
+    perspective: 1000px;
   }
 
-  /* This container is needed to position the front and back side */
   .flip-card-inner {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    text-align: center;
     transition: transform 0.8s;
     transform-style: preserve-3d;
   }
@@ -64,5 +64,47 @@
   /* Style the back side */
   .flip-card-back {
     transform: rotateY(180deg);
+  }
+  .flip-card-inner:hover .flip-card-front {
+    animation: wiggle 1000ms 1 ease-in-out;
+    -webkit-animation-name: wiggle;
+    -webkit-animation-duration: 1000ms;
+    -webkit-animation-iteration-count: 1;
+    -webkit-animation-timing-function: ease-in-out;
+  }
+  @-webkit-keyframes wiggle {
+    0% {
+      -webkit-transform: rotate(2deg);
+    }
+    25% {
+      -webkit-transform: rotate(-2deg);
+    }
+    50% {
+      -webkit-transform: rotate(4deg);
+    }
+    75% {
+      -webkit-transform: rotate(-1deg);
+    }
+    100% {
+      -webkit-transform: rotate(0deg);
+    }
+  }
+
+  @keyframes wiggle {
+    0% {
+      transform: rotate(2deg);
+    }
+    25% {
+      transform: rotate(-2deg);
+    }
+    50% {
+      transform: rotate(4deg);
+    }
+    75% {
+      transform: rotate(-1deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
   }
 </style>

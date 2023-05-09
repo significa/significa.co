a20ff814cba419ae9e96e0e3492f10f024a29515
Francisco Marques <franciscopcmarques@gmail.com>
2023-05-09 11:23:31 +0100

    feat: prettier formatting mailchimp email templates

--- scripts/update-email-templates.sh
+++ scripts/update-email-templates.sh
@@ -8,17 +8,30 @@ fi
 REGION=$(echo "$MAILCHIMP_API_KEY" | tail -c 5)
 
 function get_email_template() {
-    TEMPLATE_ID=$1
+    local template_id=$1
+
+    echo "Retrieving email template #$template_id"
 
     curl --silent --request POST \
     --url "https://${REGION}.api.mailchimp.com/2.0/templates/info" \
     --data "{
         \"apikey\": \"$MAILCHIMP_API_KEY\",
-        \"template_id\": \"$TEMPLATE_ID\"
+        \"template_id\": \"$template_id\"
     }" | jq -r '.source'
 }
 
-get_email_template 10026505 > ./src/lib/mail/template.html
-get_email_template 10026509 > ./src/lib/mail/template-minimal.html
+function update_email_template() {
+    local template_id=$1
+    local template_path=$2
+    
+    get_email_template "$template_id" > "$template_path"
+    echo "Updated $template_path with email template #$template_id"
+
+    npx prettier --write "$template_path"
+    echo "Formatted $template_path"
+}
+
+update_email_template 10026505 ./src/lib/mail/template.html
+update_email_template 10026509 ./src/lib/mail/template-minimal.html
 
 echo "Done!"

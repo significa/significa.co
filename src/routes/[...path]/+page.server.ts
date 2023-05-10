f986708df60406ad6c36e3de36b5f01f88c4eb98
Pedro Brandão <pedro@significa.co>
2023-05-10 14:44:12 +0100

    refactor: centralize load functions

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -1,22 +1,6 @@
-import { error } from '@sveltejs/kit';
-import { PREVIEW_COOKIE_KEY } from '$lib/constants';
-import { fetchPage } from '$lib/content';
-import { isr } from '$lib/isr.server.js';
+import { isr } from './isr.server';
+import { createLoad } from './load.server';
 
-export const load = async ({ params, cookies, fetch, url }) => {
-  try {
-    const page = await fetchPage({
-      slug: params.path,
-      version: cookies.get(PREVIEW_COOKIE_KEY) ? 'draft' : 'published',
-      fetch,
-      url
-    });
-
-    return { page };
-  } catch (err) {
-    console.error(err);
-    throw error(404, 'Not found');
-  }
-};
+export const load = createLoad();
 
 export const config = { isr };

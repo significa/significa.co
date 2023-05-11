0ee5f16d4c4639e35ba4dc71d3c40f89216ae2b2
kaaps <joao.guimaraes.ribeiro@gmail.com>
2023-05-11 10:52:21 +0100

    fix: services

--- src/components/pages/services.svelte
+++ src/components/pages/services.svelte
@@ -86,9 +86,9 @@
                     <div class="w-full">
                       {#if href}
                         <div class="hidden flex-1 justify-end text-foreground-tertiary xl:flex">
-                          <Button {href} variant="secondary" arrow
-                            >{award.link_text ? award.link_text : ''}</Button
-                          >
+                          <Button variant="secondary" arrow>
+                            {award.link_text ? award.link_text : ''}
+                          </Button>
                         </div>
                         <Link {href} {target} {rel} class="elevated-link" />
                       {/if}

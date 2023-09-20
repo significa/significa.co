0ba117e603ae184f8985891d137f76c78768c408
Michelle Meinert <michelle@significa.co>
2023-09-20 17:48:35 +0200

    feat: gtag

---
+++ src/components/gtag.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { dev } from '$app/environment';
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  {#if !dev && $page.data.version === 'published' && $page.url.host === 'significa.co'}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11144833772"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'AW-11144833772');
+    </script>
+  {/if}
+</svelte:head>

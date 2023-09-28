5a88fe9d05a20f2cfcf565a7c2482c06dec87a44
Julieta Frade <julieta@significa.co>
2023-09-28 17:43:20 +0100

    feat: add teams and estimates

--- src/routes/(proposals)/proposal/[slug]/proposal-page.svelte
+++ src/routes/(proposals)/proposal/[slug]/proposal-page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
-  import type { ProposalStoryblok } from '$types/bloks';
+  import type { HomePageStoryblok, ProposalStoryblok, ServicesPageStoryblok } from '$types/bloks';
   import { slugify } from '$lib/utils/paths';
   import Hero from './hero.svelte';
   import ProposalNavigation from './proposal-navigation.svelte';
   import RichText from '$components/rich-text.svelte';
   import ProposalScope from './proposal-scope.svelte';
+  import ProposalTeam from './proposal-team.svelte';
+  import ProposalEstimates from './proposal-estimates.svelte';
+  import ProposalAwards from './proposal-awards.svelte';
+  import ProposalClients from './proposal-clients.svelte';
+  import ProposalTestimonials from './proposal-testimonials.svelte';
+  import ProposalProjects from './proposal-projects.svelte';
+  import ProposalReplyBlock from './proposal-reply-block.svelte';
 
   export let proposal: ProposalStoryblok;
+  export let services: ServicesPageStoryblok;
+  export let home: HomePageStoryblok;
+
   const versions = proposal?.versions || [];
   let version = versions.length > 0 ? versions[0].version_name : '';
 
@@ -28,27 +38,55 @@
   versions={versions.map((version) => version.version_name)}
   bind:version
 />
-<div>
-  <Hero {proposal} date={content?.date}></Hero>
-  {#each content?.body || [] as section}
-    <section id={slugify(section.title)} class=" pt-20 lg:pt-28">
-      <div class="container mx-auto px-container flex flex-col gap-12 lg:flex-row">
-        <h2 bind:clientWidth={sectionTitleWidth} class="text-4xl lg:w-1/2">{section.title}.</h2>
-        <div class="w-full">
-          <RichText class="lg:w-2/3" doc={section.body} />
-        </div>
+
+<Hero {proposal} date={content?.date}></Hero>
+{#each content?.body || [] as section}
+  <section id={slugify(section.title)} class=" pt-20 lg:pt-28">
+    <div class="container mx-auto px-container flex flex-col gap-12 lg:flex-row">
+      <h2 bind:clientWidth={sectionTitleWidth} class="text-4xl lg:w-1/2">{section.title}.</h2>
+      <div class="w-full">
+        <RichText class="lg:w-2/3" doc={section.body} />
       </div>
-      <div class="pt-20">
-        {#if section.data === 'scope' && content?.scope}
-          <ProposalScope data={content.scope} {windowWidth} {containerMargin} {sectionTitleWidth} />
-        {:else if section.data === 'team' && content?.team}
-          <!-- TODO: <ProposalTeam data={content.scope} /> -->
-        {:else if section.data === 'estimates' && content?.estimates}
-          <!-- TODO: <ProposalEstimates data={content.scope} /> -->
-        {:else if section.data === 'timeline' && content?.estimates}
-          <!-- TODO: <ProposalTimeline data={content.scope} /> -->
-        {/if}
-      </div>
-    </section>
-  {/each}
-</div>
+    </div>
+    <div class="pt-20">
+      {#if section.data === 'scope' && content?.scope}
+        <ProposalScope data={content.scope} {windowWidth} {containerMargin} {sectionTitleWidth} />
+      {:else if section.data === 'team' && content?.team}
+        <ProposalTeam data={content.team} {windowWidth} {containerMargin} {sectionTitleWidth} />
+      {:else if section.data === 'estimates' && content?.estimates}
+        <ProposalEstimates
+          data={content.estimates}
+          team={content.team}
+          discount={content.discount_percentage}
+          {windowWidth}
+          {containerMargin}
+          {sectionTitleWidth}
+        />
+        <!-- FIXME: Implement timeline -->
+        <!-- {:else if section.data === 'timeline'} -->
+      {/if}
+    </div>
+  </section>
+{/each}
+
+<ProposalReplyBlock></ProposalReplyBlock>
+
+<!-- Awards -->
+{#if services.awards?.length}
+  <ProposalAwards awards={services.awards}></ProposalAwards>
+{/if}
+
+<!-- Projects -->
+{#if home.projects?.length}
+  <ProposalProjects projects={home.projects}></ProposalProjects>
+{/if}
+
+<!-- Testimonials -->
+{#if services.testimonials?.length}
+  <ProposalTestimonials testimonials={services.testimonials}></ProposalTestimonials>
+{/if}
+
+<!-- Clients -->
+{#if services.clients?.length}
+  <ProposalClients clients={services.clients}></ProposalClients>
+{/if}

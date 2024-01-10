5c3070a279e3e03562c9c86faa896bbdd6264e3a
Nuno Polonia <nunopolonia@gmail.com>
2024-01-10 13:00:29 +0000

    fix: get RSS channel data from blog storyblok SEO

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,25 +1,28 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { PREVIEW_COOKIE_KEY } from '$lib/constants';
 import { getStoryblok } from '$lib/storyblok';
 import type { BlogPostStoryblok } from '$types/bloks';
 
-export const GET: RequestHandler = async ({ cookies }) => {
+export const GET: RequestHandler = async () => {
+  const baseUrl = 'https://www.significa.co/blog/';
   const storyblok = getStoryblok();
 
   try {
     const posts: BlogPostStoryblok[] = await storyblok.getAll('cdn/stories', {
-      version: cookies.get(PREVIEW_COOKIE_KEY) ? 'draft' : 'published',
+      version: 'published',
       content_type: 'blog-post',
       sort_by: 'first_published_at:desc'
     });
+
+    const { data } = await storyblok.get('cdn/stories/blog');
+    const { seo_title, seo_description } = data.story.content;
+
+    const body = render(baseUrl, seo_title, seo_description, posts);
 
     const headers = {
       'Cache-Control': `max-age=0, s-max-age=600`,
       'Content-Type': 'application/xml'
     };
-
-    const body = render(posts);
 
     return new Response(body, { headers });
   } catch (err) {
@@ -28,22 +31,22 @@ export const GET: RequestHandler = async ({ cookies }) => {
   }
 };
 
-const render = (posts: BlogPostStoryblok[]) =>
+const render = (baseUrl: string, title: string, desc: string, posts: BlogPostStoryblok[]) =>
   `<?xml version="1.0" encoding="UTF-8" ?>
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
         <atom:link href="http://significa.co/rss.xml" rel="self" type="application/rss+xml" />
-        <title>Blog - Significa</title>
-        <link>https://www.significa.co/blog</link>
-        <description>We write stuff too. Pretty good stuff, may we say. About Design, UX/UI, Development, and, of course, Significa.</description>
+        <title>${title}</title>
+        <link>${baseUrl}</link>
+        <description>${desc}</description>
         <ttl>1800</ttl>
         ${posts
           .map(
             (post) =>
               `<item>
-              <guid>https://www.significa.co/blog/${post.slug}</guid>
+              <guid>${baseUrl}${post.slug}</guid>
               <title>${post.name}</title>
-              <link>https://www.significa.co/blog/${post.slug}</link>
+              <link>${baseUrl}${post.slug}</link>
               <description>${post?.content?.seo_description}</description>
               <pubDate>${new Date(post.published_at).toUTCString()}</pubDate>
               <category>${post.tag_list.join(', ')}</category>

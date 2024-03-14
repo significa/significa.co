22f03a278b0f26c810deefe3dc9eeaa1e65b248e
kaaps <joao.guimaraes.ribeiro@gmail.com>
2024-03-14 09:13:34 +0000

    fix: remove replaysSessionSampleRate

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -9,10 +9,6 @@ if (!dev) {
     dsn: PUBLIC_SENTRY_DNS,
     tracesSampleRate: 1.0,
 
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
-
     // If the entire session is not sampled, use the below sample rate to sample
     // sessions when an error occurs.
     replaysOnErrorSampleRate: 1.0

659554b1b6d615e9a30e70be90a4abcfd9fb1898
João Gomes <joaogomes@significa.co>
2024-10-31 18:22:19 +0000

    revert changes

--- src/lib/stores/drawer.ts
+++ src/lib/stores/drawer.ts
@@ -4,9 +4,7 @@ import { derived } from 'svelte/store';
 
 function createDrawer() {
   const { subscribe } = derived(page, ($page) => {
-    const drawerParam = $page.url.searchParams.get('drawer');
-    const parentFolderName = $page.url.pathname.split('/').pop();
-    return drawerParam && drawerParam !== parentFolderName ? drawerParam : null;
+    return $page.url.searchParams.get('drawer') || null;
   });
 
   return {
@@ -14,17 +12,15 @@ function createDrawer() {
     open: (slug: string) => {
       const url = new URL(window.location.href);
       const searchParams = new URLSearchParams(url.searchParams);
-      const parentFolderName = url.pathname.split('/').pop();
+      searchParams.set('drawer', slug);
 
-      if (slug !== parentFolderName) {
-        searchParams.set('drawer', slug);
-        goto(`${url.pathname}?${searchParams.toString()}`, { noScroll: true });
-      }
+      goto(`${url.pathname}?${searchParams.toString()}`, { noScroll: true });
     },
     close: () => {
       const url = new URL(window.location.href);
       const searchParams = new URLSearchParams(url.searchParams);
       searchParams.delete('drawer');
+
       history.back();
     }
   };

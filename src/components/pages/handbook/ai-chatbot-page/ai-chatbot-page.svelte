df36df5d2bcb098ed7e46aa72b2821019883ac33
Pedro Lunet <pedrolunet@significa.co>
2025-07-23 16:01:16 +0100

    added page template

---
+++ src/components/pages/handbook/ai-chatbot-page/ai-chatbot-page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Seo from '$components/seo.svelte';
+  import { drawerLinks } from '$lib/actions/drawer-links.js';
+</script>
+
+<Seo
+  title="placeholder"
+  description="placeholder"
+  image="placeholder"
+  structureDataMarkup="placeholder"
+/>
+<div use:drawerLinks class="container mx-auto px-container">
+  <div class="rich-text mx-auto mb-20 mt-10 max-w-2xl lg:mt-20">
+    <h1 class="[&:first-of-type]:mt-0">placeholder</h1>
+  </div>
+</div>

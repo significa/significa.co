994844cb0e3ae35ec95f52252db38a2d1b2fc75c
Pedro Lunet <pedrolunet@significa.co>
2025-07-31 15:13:12 +0100

    moved message cards to a new component

---
+++ src/components/ai-chatbot/message-card.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Shellby from '$components/ai-chatbot/shellby.svelte';
+  import SeggBinoculars from '$components/ai-chatbot/segg-binoculars.svelte';
+
+  export let messages: { type: 'user' | 'shellby'; text: string }[] = [];
+</script>
+
+{#each messages as message}
+  {#if message.type === 'user'}
+    <div class="flex items-start justify-end gap-2">
+      <div
+        class="max-w-lg rounded-[18px] border-2 border-background-offset bg-background-panel p-3"
+      >
+        <span class="text-sm">{message.text}</span>
+      </div>
+      <SeggBinoculars class="h-10 w-auto shrink-0 lg:h-12" />
+    </div>
+  {:else}
+    <div class="flex items-start gap-2">
+      <Shellby class="h-12 w-auto shrink-0 lg:h-14" />
+      <div
+        class="max-w-lg rounded-[18px] border-2 border-background-offset bg-background-panel p-3"
+      >
+        <span class="text-sm">{message.text}</span>
+      </div>
+    </div>
+  {/if}
+{/each}
--- src/components/pages/handbook/ai-chatbot-page/ai-chatbot-page.svelte
+++ src/components/pages/handbook/ai-chatbot-page/ai-chatbot-page.svelte
@@ -4,15 +4,15 @@
   import { Button, Input } from '@significa/svelte-ui';
   import clsx from 'clsx';
   import { fade } from 'svelte/transition';
-  import Shellby from '$components/ai-chatbot/shellby.svelte';
-  import SeggBinoculars from '$components/ai-chatbot/segg-binoculars.svelte';
+  import MessageCard from '$components/ai-chatbot/message-card.svelte';
 
   let searchInputValue = '';
 
   let messagesContainer: HTMLDivElement;
 
   // Message array: type = 'user' | 'shellby', text = string
-  let messages = [
+  type Message = { type: 'user' | 'shellby'; text: string };
+  let messages: Message[] = [
     {
       type: 'user',
       text: 'big user question, ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum '
@@ -53,27 +53,7 @@
   class="container mx-auto flex h-full flex-col justify-end px-container pb-0 lg:pb-8"
 >
   <div class="hide-scrollbar space-y-4 overflow-y-auto px-2 py-8" bind:this={messagesContainer}>
-    {#each messages as message}
-      {#if message.type === 'user'}
-        <div class="flex items-start justify-end gap-2">
-          <div
-            class="max-w-lg rounded-[18px] border-2 border-background-offset bg-background-panel p-3"
-          >
-            <span class="text-sm">{message.text}</span>
-          </div>
-          <SeggBinoculars class="h-10 w-auto shrink-0 lg:h-12" />
-        </div>
-      {:else}
-        <div class="flex items-start gap-2">
-          <Shellby class="h-12 w-auto shrink-0 lg:h-14" />
-          <div
-            class="max-w-lg rounded-[18px] border-2 border-background-offset bg-background-panel p-3"
-          >
-            <span class="text-sm">{message.text}</span>
-          </div>
-        </div>
-      {/if}
-    {/each}
+    <MessageCard {messages} />
   </div>
 
   <form id="search-form" class="relative flex gap-2">

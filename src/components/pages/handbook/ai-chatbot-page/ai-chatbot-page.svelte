e99ec26ab337aef5bda69673d1be4c7e6ec0ac36
Pedro Lunet <pedrolunet@significa.co>
2025-08-01 16:38:20 +0100

    now using card message component

--- src/components/pages/handbook/ai-chatbot-page/ai-chatbot-page.svelte
+++ src/components/pages/handbook/ai-chatbot-page/ai-chatbot-page.svelte
@@ -4,15 +4,15 @@
   import { Button, Input } from '@significa/svelte-ui';
   import clsx from 'clsx';
   import { fade } from 'svelte/transition';
-  import Shellby from '$components/ai-chatbot/icons/shellby.svelte';
-  import SeggBinoculars from '$components/ai-chatbot/icons/segg-binoculars.svelte';
+  import MessageCard from '$components/ai-chatbot/message-card.svelte';
 
   let searchInputValue = '';
 
   let messagesContainer: HTMLDivElement;
 
   // Message array: type = 'user' | 'shellby', text = string
-  let messages = [
+  type Message = { type: 'user' | 'shellby'; text: string };
+  let messages: Message[] = [
     {
       type: 'user',
       text: 'big user question, ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ipsum dolor sit amet? ipsum dolor sit amet, consectetur adipiscing elit. Ipsum '
@@ -49,25 +49,7 @@
 >
   <div class="hide-scrollbar space-y-4 overflow-y-auto px-2 py-8" bind:this={messagesContainer}>
     {#each messages as message}
-      {#if message.type === 'user'}
-        <div class="flex items-start justify-end gap-2">
-          <div
-            class="max-w-lg rounded-[18px] border-2 border-background-offset bg-background-panel p-3"
-          >
-            <span class="text-sm">{message.text}</span>
-          </div>
-          <SeggBinoculars class="h-10 w-auto shrink-0 lg:h-12" />
-        </div>
-      {:else}
-        <div class="flex items-start gap-2">
-          <Shellby class="h-12 w-auto shrink-0 lg:h-14" />
-          <div
-            class="max-w-lg rounded-[18px] border-2 border-background-offset bg-background-panel p-3"
-          >
-            <span class="text-sm">{message.text}</span>
-          </div>
-        </div>
-      {/if}
+      <MessageCard type={message.type} text={message.text} />
     {/each}
   </div>
 

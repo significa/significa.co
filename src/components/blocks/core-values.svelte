63f93066818fb1773df157ff487de49be070c2af
João Gomes <joaogomes@significa.co>
2026-01-23 11:21:53 +0000

    feat(core-values): add support for images

--- src/components/blocks/core-values.svelte
+++ src/components/blocks/core-values.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import ValueIllustrations from '$components/pages/about/value-illustrations.svelte';
   import { storyblokEditable } from '$lib/actions/storyblok-editable';
+  import { getImageAttributes } from '$lib/utils/cms';
   import type { CoreValuesStoryblok } from '$types/bloks';
 
   export let block: CoreValuesStoryblok;
@@ -26,10 +26,11 @@
           <div
             class="flex flex-col items-start gap-4 border-t py-8 first:border-none lg:flex-row lg:items-center lg:gap-8 lg:py-12"
           >
-            <ValueIllustrations
-              variant={value.illustration}
-              class="block shrink-0 drop-shadow-md"
-            />
+            {#if value.image?.filename}
+              {@const { src, alt, width, height } = getImageAttributes(value.image)}
+              <img class="h-auto w-[120px] shrink-0" {src} {alt} {width} {height} />
+            {/if}
+
             <div>
               <h3 class="text-2xl font-semibold">{value.title}</h3>
               <p class="text-2xl text-foreground-secondary">{value.description}</p>

fb5b55ea279d16dc5daf24a351386ebbe83fca78
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2026-02-19 09:32:30 +0000

    add import alias

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
-import { getPublishedPosts } from "../../lib/collections";
+import { getPublishedPosts } from "@/lib/collections";
 
 export async function GET(context: APIContext) {
   const posts = await getPublishedPosts();

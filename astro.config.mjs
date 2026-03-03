b73592a514d4bf2d8bdb4c22fd7f3bb741455b79
Tomás Gouveia <tomasgouveia@significa.co>
2026-03-03 15:12:03 +0000

    Menu navigation and Switzer Font implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
+
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://significa.co",
-  integrations: [mdx(), react(), sitemap()],
+  integrations: [mdx(), sitemap()],
 });

d946ca1fe2e988333fda5448650e6b2ebe383a07
Tomás Gouveia <tomasgouveia@significa.co>
2026-03-12 16:33:33 +0000

    blogposts migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
   site: "https://significa.co",
   integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["noopener", "noreferrer"],
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
